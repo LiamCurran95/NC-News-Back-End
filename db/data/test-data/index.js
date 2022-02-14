de61b92da88703ed753488b584c25f00769cf033
LiamCurran <LiamCurran95>
2022-02-14 11:45:28 +0000

    Changing back index.js for dev/test data

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -3,11 +3,4 @@ exports.commentData = require("./comments.js");
 exports.topicData = require("./topics.js");
 exports.userData = require("./users.js");
 
-const data = {
-	articleData: this.articleData,
-	commentData: this.commentData,
-	topicData: this.topicData,
-	userData: this.userData,
-};
-
 module.exports = data;
