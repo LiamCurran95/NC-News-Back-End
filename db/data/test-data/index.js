b5de0baabe4ee04cfc52ecd52ca95022e400f2ab
LiamCurran <LiamCurran95>
2022-02-14 11:42:12 +0000

    Adding data obj to data/test index.js

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,4 +1,13 @@
-exports.articleData = require('./articles.js');
-exports.commentData = require('./comments.js');
-exports.topicData = require('./topics.js');
-exports.userData = require('./users.js');
+exports.articleData = require("./articles.js");
+exports.commentData = require("./comments.js");
+exports.topicData = require("./topics.js");
+exports.userData = require("./users.js");
+
+const data = {
+	articleData: this.articleData,
+	commentData: this.commentData,
+	topicData: this.topicData,
+	userData: this.userData,
+};
+
+module.exports = data;
