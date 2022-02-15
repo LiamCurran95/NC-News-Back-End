c0ea80eed9c38c476f13837a407dcd271f31594d
LiamCurran <LiamCurran95>
2022-02-15 11:32:44 +0000

    Core task 14 complete

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,8 +1,32 @@
 const db = require("../db/connection");
 
-exports.fetchArticles = () => {
-	let databaseQuery = `SELECT * FROM articles`;
-	return db.query(databaseQuery).then(({ rows }) => {
-		return rows;
-	});
+exports.checkArticleExists = (id) => {
+	return db
+		.query(`SELECT * FROM articles WHERE article_id = $1`, [id])
+		.then(({ rows }) => {
+			if (rows.length === 0) {
+				return Promise.reject({
+					status: 404,
+					msg: "Valid ID format, article does not exist",
+				});
+			} else {
+				return rows[0];
+			}
+		});
+};
+
+exports.fetchArticlesById = (id) => {
+	return db
+		.query(
+			`SELECT username, title, article_id, body, topic, created_at, votes 
+    FROM articles
+    JOIN users
+    ON users.username = articles.author
+    WHERE article_id = $1;`,
+			[id]
+		)
+		.then(({ rows }) => {
+			console.log(rows);
+			return rows;
+		});
 };
