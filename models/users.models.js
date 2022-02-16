f14e4e3540fde2d237381405d2b61c6105e183bf
LiamCurran <LiamCurran95>
2022-02-16 10:39:11 +0000

    Responding to comments

--- models/users.models.js
+++ models/users.models.js
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 
 exports.fetchUsers = (query) => {
-	let databaseQuery = `SELECT username FROM users`;
+	let databaseQuery = `SELECT username FROM users;`;
 	return db.query(databaseQuery).then(({ rows }) => {
 		return rows;
 	});
