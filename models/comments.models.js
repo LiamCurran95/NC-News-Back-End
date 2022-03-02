897747586062dc953e1e1e96083b25d4ebd3bf24
LiamCurran <LiamCurran95>
2022-03-02 18:59:46 +0000

    Complete core task 12

---
+++ models/comments.models.js
@@ -0,0 +1,17 @@
+const db = require("../db/connection");
+
+exports.deleteCommentById = (commentId) => {
+	console.log(commentId);
+	return db
+		.query("DELETE FROM comments WHERE comment_id = $1 RETURNING *;", [
+			commentId,
+		])
+		.then(({ rows: [comment] }) => {
+			if (!comment)
+				return Promise.reject({
+					status: 404,
+					msg: "No comment found for this ID.",
+				});
+			return comment;
+		});
+};
